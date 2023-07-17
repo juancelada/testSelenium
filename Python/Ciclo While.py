df735e7d04243b45134315d20f66bdf024c1fa0f
juancelada <108694741+juancelada@users.noreply.github.com>
2023-07-17 15:58:25 -0300

    Create Ciclo While.py

---
+++ Python/Ciclo While.py
@@ -0,0 +1,10 @@
+ini=0
+fin=4
+
+while(ini<=fin):
+    ini=ini+1           #sirve como contador para que a medida que se vaya ejecutando el while se le vaya sumando una unidad a la variable inicio
+    if(ini==3):
+        continue
+    print("hola" + str(ini))
+
+
