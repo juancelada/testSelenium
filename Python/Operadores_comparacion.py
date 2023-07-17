8925b11ded4ec8fb6a7878a8ffe6f4ddd775cd48
juancelada <108694741+juancelada@users.noreply.github.com>
2023-07-17 15:58:43 -0300

    Create Operadores_comparacion.py

---
+++ Python/Operadores_comparacion.py
@@ -0,0 +1,16 @@
+a=6
+b=5
+c=9
+
+print("son iguales? " + str(a==b))       #estas funciuones sirven para hacer comparaciones
+print("10 es menor que 5 " + str(a<b))
+print("10 es mayor que 5 " + str(a>b))
+print("10 es menor o igual que 5 " + str(a<=b))
+print("10 es mayor o igual que 5 " + str(a>=b))
+print("10 es diferente que 5 " + str(a!=b))
+
+
+print("a es menor que b y c: " + str(a<b and b<c))
+
+print("a es menor que b y c: " + str(a<b or b<c))
+
