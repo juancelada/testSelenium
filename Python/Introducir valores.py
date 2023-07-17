663695468f10c862b96f40dac761f2c9825f0088
juancelada <108694741+juancelada@users.noreply.github.com>
2023-07-17 15:58:40 -0300

    Create Introducir valores.py

---
+++ Python/Introducir valores.py
@@ -0,0 +1,21 @@
+print("Ingresa tu nombre")
+nom = input()
+
+print("Ingresa tu Dni")
+dni = input()
+
+print("Tu nombre es: {} y tu DNI es: {}" .format(nom,dni))
+
+print("dame el valor de A")
+a=input()
+a=int(a)
+
+print("dame el valor de B")
+b=input()
+b=int(b)
+
+suma=a+b
+
+print("La suma de A y B es igual a: "+str((a+b))) #una manera de sumar
+
+print("La suma de A y B es igual a: {}" .format(suma))
